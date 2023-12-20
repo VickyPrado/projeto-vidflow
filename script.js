30cbf3064e587411861a51d55f18f53307448061
VickyPrado <vicky.prado05@gmail.com>
2023-12-20 12:09:43 -0300

    exibindo-videos

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+const containerVideos = document.querySelector(".videos__container");
+
 const api = fetch("http://localhost:3000/videos")
-.then(res => console.log(res.json()))
+.then(res => res.json())
+.then ((videos) =>
+    videos.forEach((video)=> {
+        containerVideos.innerHTML += `
+        <li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem} alt="Logo do canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>    
+            </div>
+        </li>
+        `;
+    })
+)
+    
