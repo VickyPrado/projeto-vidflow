7fb72cc344813ee56665e59f6a21567947534d82
VickyPrado <vicky.prado05@gmail.com>
2023-12-20 14:05:34 -0300

    refatorando-cod-barra-de-pesquisa

--- script.js
+++ script.js
@@ -28,22 +28,14 @@ const barraDePesquisa = document.querySelector(".pesquisar__input");
 
 barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
-function filtrarPesquisa(){
-    const videos = document.querySelectorAll(".videos__item");
-
-    if(barraDePesquisa.value != ""){
-        for(let video of videos){
-            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
-            let valorFiltro = barraDePesquisa.value.toLowerCase();
-            
-            if(!titulo.includes(valorFiltro)){
-                video.style.display = "none"; 
-            } else{
-                video.style.display = "block";
-            }
-        } 
-    } else {
-        video.style.display = "block";
-    }
-}
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+  
+    videos.forEach((video) => {
+      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+  
+      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+  }
     
