153dadaf7c54bd1cfb4888971986f41ed91d7cc1
VickyPrado <vicky.prado05@gmail.com>
2023-12-20 15:52:58 -0300

    organizando-cod

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const containerVideos = document.querySelector(".videos__container");
 
+
 async function buscarEMostrarVideos(){
     try{
         const busca = await fetch("http://localhost:3000/videos");
         const videos = await busca.json();
-        
+
             videos.forEach((video)=> {
+                if(video.categoria == ""){
+                    throw new Error('Vídeo não tem categoria');
+                }
                 containerVideos.innerHTML += `
                 <li class="videos__item">
                     <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
                     <div class="descricao-video">
-                        <img class="img-canal" src="${video.imagem} alt="Logo do canal">
+                        <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
-                        <p class="titulo-canal">${video.descricao}</p>    
+                        <p class="titulo-canal">${video.descricao}</p>
                         <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
                 `;
             })
     } catch(error){
-        containerVideos.innerHTML = `<p>Houve um erro ao carregar os videos: ${error}</p>`
-    }      
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
+    }
 }
 
 buscarEMostrarVideos();
@@ -43,8 +47,8 @@ function filtrarPesquisa() {
 const botaoCategoria = document.querySelectorAll(".superior__item");
 
 botaoCategoria.forEach((botao) => {
-   let nomeCategoria = botao.getAttribute("name");
-   botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
 })
 
 function filtrarPorCategoria(filtro){
